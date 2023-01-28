e713609d4a782165e005905c3355f4cd0148485a
abnergit <denysabner@gmail.com>
2023-01-28 12:56:53 -0300

    Update dns_capture.py

--- dns_capture.py
+++ dns_capture.py
@@ -5,17 +5,29 @@ try:
 except ImportError:
     subprocess.run(["pip", "install", "scapy"])
     from scapy.all import *
-
+listados = []
 def dns_sniff(pkt):
     # Check if the packet is a DNS packet
     if pkt.haslayer(DNSQR):
         # Extract the source IP address
-        src_ip = pkt[IP].src
-        # Extract the destination IP address
-        dst_ip = pkt[IP].dst
+        try:
+	        src_ip = pkt[IP].src
+        except:
+	        src_ip = "Nao identificado"
+
+	# Extract the destination IP address
+        try:
+            dst_ip = pkt[IP].dst
+        except:
+	        dst_ip = "Nao identificado"
         # Extract the domain name
         domain_name = pkt[DNS].qd.qname.decode()
-        print(f"Source IP: {src_ip} Destination IP: {dst_ip} Domain Name: {domain_name}")
+        tipo = "Nao identificado"
+        if IP in pkt:
+            tipo = pkt[IP].version
+        if domain_name not in listados:
+            print(f"Version: {tipo} Source IP: {src_ip} Destination IP: {dst_ip} Domain Name: {domain_name}")
+            listados.append(domain_name)
 
 # Sniff packets on the wlo1 interface
 sniff(iface="wlo1", filter="udp port 53", prn=dns_sniff)
